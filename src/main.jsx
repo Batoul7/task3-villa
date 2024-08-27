b742a7735286162a5b102bc26a31899251b1931b
Batoul7 <batoulab268@gmail.com>
2024-08-27 11:17:57 +0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='task3-villa'>
+    <BrowserRouter basename='/task3-villa'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
